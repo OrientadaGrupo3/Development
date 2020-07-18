9e98fe79960e402bfb90a45983aa1e86a03f627f
grevin.solorzano25@gmail.com <>
2020-07-17 18:03:36 -0600

    Tarea Grupal. Yo Grevin Solorzano Hice la clase Doctor Persona Y profecion

---
+++ src/profesion/Doctor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profesion;
+
+import persona.persona;
+
+/**
+ *
+ * @author DELL
+ */
+public class Doctor extends persona{
+     public Doctor() {
+        
+        setNombre("Grevin Solorzano");
+        setEdad(25);
+        setTrabajo("Catarino Rivas");
+    
+}
+    
+}
